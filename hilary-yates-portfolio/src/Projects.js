cdaf58b94361ac4f2a4fa6fc326560cccd5d5b4b
hilaryyates <hilryyates@gmail.com>
2022-12-05 08:34:56 -0500

    test

--- hilary-yates-portfolio/src/Projects.js
+++ hilary-yates-portfolio/src/Projects.js
@@ -3,6 +3,7 @@ import react from "react";
 const Projects = () => {
   return (
     <div>
+      <h1>test</h1>
       <h1 className='projects-header' id='projects'>
         Some of my projects
       </h1>
